43a8315a246e214bd49eeb1260b805e185ec801b
Simon <simonanewton@gmail.com>
2020-05-28 19:50:28 -0400

    1.2 added MySQL methods to orm object

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,52 @@
 const connection = require("./connection");
 
-const selectAll = () => {
-
-};
-
-const insertOne = () => {
-
-};
-
-const updateOne = () => {
-
-};
-
 const orm = {
-    selectAll: selectAll,
-    insertOne: insertOne,
-    updateOne: updateOne
+    selectAll: () => {
+        const query = "SELECT * FROM burgers";
+
+        return new Promise((resolve) => {
+            connection.query(query, (err, res) => {
+                if (err) throw err;
+                resolve(res);
+            });
+        });
+    },
+
+    insertOne: (burger) => {
+        const query = "INSERT INTO burgers SET ?";
+        const values = [
+            {
+                burger_name: burger.name,
+                devoured: burger.devoured
+            }
+        ];
+
+        return new Promise((resolve) => {
+            connection.query(query, values, (err, res) => {
+                if (err) throw err;
+                resolve(res);
+            });
+        });
+    },
+
+    updateOne: (burger) => {
+        const query = "UPDATE burgers SET ? WHERE ?";
+        const values = [
+            {
+                devoured: burger.devoured
+            },
+            {
+                burger_name: burger.name
+            }
+        ];
+
+        return new Promise((resolve) => {
+            connection.query(query, values, (err, res) => {
+                if (err) throw err;
+                resolve(res);
+            });
+        });
+    }
 };
 
 module.exports = orm;
